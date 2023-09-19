06a0ccf93a9e421bf93456645d41b943d21e194a
DreaMTH <d.dimcard@gmail.com>
2023-09-19 23:14:58 +0300

    AoAoAoAo

--- main.cpp
+++ main.cpp
@@ -1,33 +1,70 @@
 #include <iostream>
-
-int findP(int);
-int findP(int, int);
+#include <vector>
+long ludicNumbers(long*, long*, long, long, long);
+long ludicNumbers(std::vector<long>&, std::vector<long>&, long, long, long);
+void initializeArray(long*&, long);
 int main(int argc, const char* argv[]){
-    int N;
-    int K; 
-    int P;
+    long N; //amount of elements
     std::cin >> N;
-    std::cin >> K;
-    P = findP(N);
-    //std::cout << P;
-    P = findP(4, K);
-    std::cout << P;
-    std::shared_ptr<int> ptr = std::make_shared<int>(N);
-    std::cout << *ptr;
-}
+    long*a;
+    long*b;
+    long na;
+    initializeArray(a, N);
+    b = new long[N];
+    na = N;
+    
+    std::vector<long> x(N);
+    std::vector<long> y(N);
+    /*
+    for(long i = 1; i < N; i++){
+        x[i] = i;
+    }*/
+    
+    
+    for(long i = 2; i < na && i+a[i]<na;i++){
+        na = ludicNumbers(a, b, i, a[i], na);
+    }
+    /*
+      for(long i = 2; i < na && i+x[i]<na;i++){
+        na = ludicNumbers(x, y, i, x[i], na);
+    }*/
+    std::cout << na - 1 << '\n';
 
-int findP(int N){
-    return 0;
-}
+    if(a != nullptr){
+        delete[]a;
+    }
+    if(b != nullptr){
+        delete[]b;
+    }
 
-int findP(int N, int K){
-    int koef = 1;
-    int i;
-    if (N - K > K)
-      K = N - K;
-    for (i = K + 1; i <= N; i++)
-      koef = koef * i;
-    for (i = 1; i < (N - K + 1); i++)
-      koef = koef / i;
-    return koef;
+}
+void initializeArray(long *&array, long N){
+    array = new long[N];
+    for(long i = 1; i < N; i++){
+        array[i] = i;
+    }
+}
+long ludicNumbers(long *items, long *outArr, long b, long n, long na){
+    long nb = 1;
+
+    for(long i = 1; i < na; i++){
+        if(i != b && i%n == b%n){continue;}
+        outArr[nb++] = items[i]; 
+    }
+    for(long i = 1; i < na; i++){
+        items[i] = outArr[i];
+    }
+    return nb;
+}
+long ludicNumbers(std::vector<long> &vc, std::vector<long>&bc, long b, long n, long na){
+    long nb = 1;
+
+    for(long i = 1; i < na; i++){
+        if(i != b && i%n == b%n){continue;}
+        bc[nb++] = vc[i]; 
+    }
+    for(long i = 1; i < na; i++){
+        vc[i] = bc[i];
+    }
+    return nb;
 }
